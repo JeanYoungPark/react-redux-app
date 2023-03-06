6eb30e4ec00275e46901377df569491b946556a2
jjin0ee <jjin0ee@littlefox.com>
2023-03-06 13:57:29 +0900

    combineReducer

---
+++ src/reducers/counter.tsx
@@ -0,0 +1,16 @@
+interface Action {
+    type: string
+}
+const counter = (state = 0, action: Action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return state + 1;
+            break;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+export default counter;

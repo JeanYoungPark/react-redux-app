3b423a1e173db9a31435331d1554c08b08da2d6b
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2023-03-07 21:57:03 +0900

    useSelector, useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./reducers/todos";
 
 type Props = {
   value: any;
   onIncrement: () => void;
   onDecrement: () => void;
-}
+};
 
-function App({value, onIncrement, onDecrement}: Props) {
-const [todoValue, setTodoValue] = useState("");
-const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setTodoValue(e.target.value);
-}
-const addTOdo = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  setTodoValue('');
-}
+function App({ value, onIncrement, onDecrement }: Props) {
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+  const counter = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", text: todoValue });
+    setTodoValue("");
+  };
 
   return (
     <div className="App">
-      {/* Clicked: {value} times */}
-      <button onClick={onIncrement}>
-        +
-      </button>
-      <button onClick={onDecrement}>
-        -
-      </button>
-
+      Clicked: {counter} times
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
       <form onSubmit={addTodo}>
-        <input type="text" value={todoValue} onChange={handleChange}/>
-        <input type="submit"/>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
       </form>
     </div>
   );
--- src/reducers/todos.tsx
+++ src/reducers/todos.tsx
@@ -1,21 +1,24 @@
+import rootReducer from ".";
+
 enum ActionType {
-    ADD_TODO = "ADD_TODO",
-    DELETE_TODO = "DELETE_TODO"
+  ADD_TODO = "ADD_TODO",
+  DELETE_TODO = "DELETE_TODO",
 }
 
 interface Action {
-    type: ActionType
-    text: string
+  type: ActionType;
+  text: string;
 }
 
 const todos = (state = [], action: Action) => {
-    switch (action.type) {
-        case "ADD_TODO":
-            return [...state, action.text];
-            break;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, action.text];
+      break;
+    default:
+      return state;
+  }
+};
 
 export default todos;
+export type RootState = ReturnType<typeof rootReducer>;
